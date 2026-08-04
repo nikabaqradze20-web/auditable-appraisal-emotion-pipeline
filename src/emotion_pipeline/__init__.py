ebbace9c6cf1d0c577943c7827603d00613078b9
nikabaqradze20-web <nikabaqradze20@gmail.com>
2026-08-04 13:40:14 +0200

    Polish public repository presentation

--- src/emotion_pipeline/__init__.py
+++ src/emotion_pipeline/__init__.py
@@ -1,4 +1,4 @@
-"""Offline reference implementation of the auditable annotation workflow."""
+"""Offline, Layer 2-only reference implementation of the auditable workflow."""
 
 from .pipeline import run_pipeline
 
